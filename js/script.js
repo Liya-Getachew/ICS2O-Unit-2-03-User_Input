cdeca5f1db1237bd641f8815bc2e503965e71a85
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-12 18:10:52 -0400

    space

--- js/script.js
+++ js/script.js
@@ -14,5 +14,5 @@ function enterButtonClicked() {
 
   // output
   document.getElementById("address").innerHTML =
-    "Your address is: " + streetNumber + streetName + "."
+    "Your address is: " + streetNumber + " " + streetName + "."
 }
